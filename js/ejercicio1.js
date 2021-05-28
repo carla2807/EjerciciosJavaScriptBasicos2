7bd656dc93f21eea2643ec0538372230b0ee9d29
carla2807 <77696776+carla2807@users.noreply.github.com>
2021-05-28 17:44:42 -0300

    Ejercicio 1

---
+++ js/ejercicio1.js
@@ -0,0 +1,51 @@
+/* 
+1.- Completar las condiciones de los if del siguiente script para que los mensajes de los
+alert se muestren de forma correcta.
+var num1 = 5;
+var num2 = 8;
+if(...) {
+alert("num1 no es mayor que num2");
+}
+if(...) {
+alert("num2 es positivo");
+}
+if(...) {
+alert("num1 es negativo o distinto de cero");
+}
+if(...) {
+alert("Incrementar en 1 unidad el valor de num1 lo hace mayor que num2");
+}*/
+
+
+var num1 = 5;
+var num2 = 8;
+
+if(num1<num2){
+alert("num1 no es mayor que num2");
+}
+
+var numero1 = -5;
+var numero2 = 8;
+
+if(numero1<0) {
+    alert("numero2 es positivo");
+}
+
+var numero1 = -5;
+var numero2 = 8;
+
+
+var Numero1 = -5;
+var Numero2 = 8;
+
+if(Numero1<0 || Numero1!=0) {
+    alert("Numero1 es negativo o distinto de cero");
+    }
+
+var nm1= 8;
+var nm2= 5;
+
+
+if(nm1++) {
+    alert("Incrementar en 1 unidad el valor de nm1 lo hace mayor que nm2");
+}
